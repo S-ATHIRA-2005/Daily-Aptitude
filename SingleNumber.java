28d0e38401d5f599f4978a57427453fcae5e197b
S-ATHIRA-2005 <athirasekhar18@gmail.com>
2026-08-30 13:48:02 +0530

    Add files via upload

---
+++ SingleNumber.java
@@ -0,0 +1,16 @@
+public class SingleNumber {
+    public static void main(String[] args) {
+
+        int[] nums = {4, 1, 2, 1, 2};
+
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            result = result ^ nums[i];
+        }
+
+        System.out.println("Single Number :" + result);
+    }
+}
+    
+
