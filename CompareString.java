cfc9d7f23039d30e726f47e987c0608ee507ea4d
S-ATHIRA-2005 <athirasekhar18@gmail.com>
2026-09-20 09:14:54 +0530

    Add files via upload

---
+++ CompareString.java
@@ -0,0 +1,8 @@
+public class CompareString {
+    public static void main(String[] args) {
+        //"=="  ".equals()"
+        String s1=new String("Pooja");
+        String s2="Pooja";boolean ise
+    }
+    
+}
