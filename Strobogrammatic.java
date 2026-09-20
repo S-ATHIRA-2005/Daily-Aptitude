e1800737ba3526d36348a341d9b2fedee1e9d682
S-ATHIRA-2005 <athirasekhar18@gmail.com>
2026-09-20 09:21:55 +0530

    Add files via upload

---
+++ Strobogrammatic.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+public class Strobogrammatic {
+    public static boolean isStrobogrammatic(String s){
+        HashMap<Character,Character>map=newHashMap<>();
+        map.put('0','0');
+        map.put('1','1');
+        map.put('6','9');
+        map.put('8','8');
+        map.put('9','6');
+        int l=0;r=
+        
+
+    }
+
+    
+}
