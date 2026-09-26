862e6d3ad6a1acfb67b2d23b4d32712dab491d01
S-ATHIRA-2005 <athirasekhar18@gmail.com>
2026-09-26 07:24:19 +0530

    Add files via upload

---
+++ MissingNumber.java
@@ -0,0 +1,23 @@
+public class MissingNumber {
+
+    public static int missingNumber(int[] nums) {
+
+        int n = nums.length;
+        int sum = n * (n + 1) / 2;
+
+        for (int num : nums) {
+            sum = sum - num;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {3, 0, 1};
+
+        int answer = missingNumber(nums);
+
+        System.out.println("Missing Number = " + answer);
+    }
+}
